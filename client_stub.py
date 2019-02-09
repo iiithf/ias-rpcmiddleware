1a63b652b2dba7fd93f02b7c735e399b267e09ef
Subhajit Sahu <wolfram77@gmail.com>
2019-02-10 01:28:02 +0530

    :mushroom: client with validation

--- client_stub.py
+++ client_stub.py
@@ -1,14 +1,46 @@
 from http.client import HTTPConnection
 import json
+import re
+
+
+def parse_args(parts):
+  (typ, req) = ({}, set())
+  for i in range(2, len(parts), 2):
+    arg = re.sub(r'\W', '', parts[i+1])
+    typ[arg] = parts[i]
+    if arg == parts[i+1]:
+      req.add(arg)
+  return (typ, req)
+
+def validate_typ(nam, val, typ):
+  if typ != type(val):
+    raise TypeError(nam+'='+val+' ('+type(val)+'), but expected '+typ)
+  return
+
+def validate_args(data, typ, req):
+  for k, v in data.items():
+    validate_typ(k, v, typ[k])
+  for k in req:
+    if data[k] is None:
+      raise ValueError(k+' is required')
+  return
 
 
 class ClientStub:
-  def call(self, name, data):
-    conn = HTTPConnection('127.0.0.1', port=1992)
-    data = {'begin':1, 'end': 2}
-    conn.request('POST', '/json', body=json.dumps(data))
-    res = conn.getresponse()
-    print(json.loads(res.read()))
+  def __init__(self, sign, srvc='default', host='127.0.0.1', port=1992):
+    parts = re.sub(r'[^\w\[\]]+', ' ', sign).split()
+    (self.args_typ, self.args_req) = parse_args(parts)
+    [self.retn_typ, self.func] = parts[0:1]
+    self.srvc = srvc
+    self.host = host
+    self.port = port
 
-a = ClientStub()
-a.call('a', {})
+  def call(self, data):
+    validate_args(data, self.args_typ, self.args_req)
+    conn = HTTPConnection(self.host, self.port)
+    url = '/'+self.srvc+'/'+self.func
+    conn.request('POST', url, body=json.dumps(data))
+    res = conn.getresponse()
+    data = json.loads(res.read())
+    validate_typ('return', data, self.retn_typ)
+    return data
